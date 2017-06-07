2dac2d6a1de7dec60dc1a6a063a63a4afa29bbea
Morgan Gangwere <morgan.gangwere@gmail.com>
2017-06-06 17:13:40 -0700

    Initial commit

---
+++ LibServer/views.py
@@ -0,0 +1,255 @@
+"""
+BoxNG admin panel.
+"""
+
+from datetime import datetime
+from flask import render_template, abort, redirect
+from LibServer import app
+
+from configparser import ConfigParser
+
+#
+# This is the simplest route in the entire app.
+# All this does is show some details about what's turned on and what
+# isn't. 
+#
+
+@app.route('/')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+    )
+
+
+# Configuration sections
+
+configuration_groups = {
+    "general":{
+        "name":"General configuration",
+        "short":"General",
+        "desc":"Overall configuration of your box",
+        "icon":"fa fa-cog",
+        "section":"general",
+        "type":"single",
+        "template":"cfg_general.html"
+        },
+    "network": {
+        "name":"Network (general)",
+        "short":"Network",
+        "desc":"Configure DNS and other general network configuration",
+        "icon":"fa fa-wrench",
+        "type":"single",
+        "template":"cfg_network.html",
+        "section":"network",
+        "leaf":"Network"
+        },
+    "network_interface":{
+        "name":"Interface Configuration",
+        "short":"Network Interfaces",
+        "desc":"Configure how your box uses its network interfaces",
+        "icon":"fa fa-microchip",
+        "namespace": "interface",
+        "type":"multi",
+        "template":None,
+        "leaf":"Interface"
+        },
+    "services":{
+        "name":"Service Configuration",
+        "short":"Services",
+        "desc":"Services you can run on your box",
+        "icon":"fa fa-server",
+        "namespace":"service",
+        "type":"multi",
+        "template":"cfg_service.html",
+        "breadcrumb":"Service"
+        },
+    "storage":{
+        "name":"Storage configuration",
+        "short":"Storage",
+        "desc":"Content storage for your users",
+        "type":"single",
+        "icon":"fa fa-hdd-o",
+        "section": "storage",
+        "template":"cfg_storage.html",
+        "breadcrumb":"Location"
+        }
+    }
+
+
+@app.route("/config")
+def config_index():
+    """
+    this route only lists the available sections.
+    """
+    return render_template("config/index.html",
+                           sections=configuration_groups
+                           )
+
+def get_config_reader():
+    conf_parser = ConfigParser();
+    conf_parser.read("config.ini")
+
+    return conf_parser
+
+
+def get_group_sections(config_reader, group):
+    """
+    Gets the sections that are relevant to a specific group.
+    """
+
+    # If the group we're being asked for doesn't exist, return None
+    # This is mostly a sanity check.
+    if group not in configuration_groups.keys():
+        return None
+    # Get the list of sections that are within this group's namespace
+    this_group = configuration_groups[group]
+    
+    # Sanity check: Is the group we're working a multi?
+    # Plurals can only be used by multi-section groups.
+    if this_group["type"] != "multi":
+        return None
+
+    # now, we're going to find the sections that match the group's prefix.
+    filter_func = lambda section: section.startswith(this_group["namespace"]+".")
+
+    this_group_sections = list(filter(filter_func, config_reader.sections()))
+
+    # We need a small amount of post-processing here, however.
+    this_group_subpages = []
+    for section in this_group_sections:
+        # do a thing
+        this_subpage = {
+            "name":section,
+            "group":group
+            }
+        prefixlen=len(this_group["namespace"])+1
+        # we need to fill in `label` as appropriate.
+        shortname = section[prefixlen::]
+        if config_reader.has_option(section, "type"):
+            this_subpage["template"] = "cfg_{0}_{1}.html".format( this_group["namespace"], config_reader.get(section, "type"))
+        else:
+            this_subpage["template"] = this_group["template"]
+        if config_reader.has_option(section, "ui_label"):
+            this_subpage["label"] = "{0} ({1})".format(config_reader.get(section, "ui_label"), shortname)
+        else:
+            this_subpage["label"] = shortname
+        this_group_subpages.append(this_subpage)
+    return this_group_subpages
+
+
+
+
+@app.route("/config/<group>")
+def config_group(group):
+    """
+    this route gets a specific section. If that section is a single section, it shows it.
+    otherwise, it lists the known configuration points within that section. 
+    """
+
+    # TODO: return a 404. 
+    if group not in configuration_groups.keys():
+        abort(404)
+
+    group_dict = configuration_groups[group]
+
+    conf_parser = get_config_reader();
+
+
+    if group_dict["type"] == "multi":
+
+        current_subpages = get_group_sections(conf_parser, group)
+
+        # return the rendered page.
+        return render_template(
+            'config/base.html',
+            current_page=group,
+            config_pages=configuration_groups,
+            current_subpage=None,
+            subpages = current_subpages
+            )
+    else:
+        config_values = dict(conf_parser.items(group_dict["section"]))
+
+        try:
+            # Attempt to render the configuration group's page.
+            return render_template(
+                "config/"+config_group["template"],
+                current_page=group,
+                config_pages=configuration_groups,
+                values=config_values,
+                ini_section=group
+                )
+        except:
+            return render_template(
+                "config/cfg_generic.html",
+                current_page=group,
+                config_pages=configuration_groups,
+                values=config_values,
+                ini_section=group
+                )
+
+    return render_template(
+        'config/base.html',
+        current_page=group,
+        config_pages=configuration_groups
+        )
+
+@app.route("/config/<group>/<item>")
+def config_item(group, item):
+    config_reader = get_config_reader();
+
+    # get the current page.
+
+    subpages = get_group_sections(config_reader, group)
+
+    # todo: this is magic. document it.
+
+    tSubpage = list(filter(lambda x: x["name"] == item and x["group"] == group, subpages )) 
+    if(len(tSubpage)>1):
+        abort(500)
+    else:
+        tSubpage = tSubpage[0]
+
+    # We need the current state of the configuration of that section
+
+    config_options = dict(filter( lambda i: i[0] != 'ui_label' and i[0] != 'type', list(config_reader.items(item))))
+
+    possible_templates = (tSubpage["template"], configuration_groups[group]["template"], "cfg_generic.html")
+
+    for template in possible_templates:
+        try:
+            return render_template(
+                "config/"+template,
+                current_page=group,
+                current_subpage = item,
+                config_pages = configuration_groups,
+                subpages = subpages,
+                values = config_options,
+                ini_section=item
+                )
+        except:
+            continue
+    # this should not be a 5xx -- should it?
+    abort(500)
+
+
+@app.route('/config/write/<section>', methods=['POST'])
+def write_config(section):
+    """
+    this method takes a section and writes it to the configuratiom file as requested.
+
+    we can get away with this because we read the configuration every time we load a page. 
+    this means we always have a fresh copy of the configuration. 
+
+    TODO: we need to implement this. This needs the app search paths work done so this works.
+    that will give us the file we want to write to, as the primary config is likely in a read only place.
+
+    TODO: does this need to know the section before hand or can we pass that in via the POST data?
+    I don't think it's a bad idea to have it as part of the url part. This would make the request a little nicer looking. It also has the advantage of making it possible to
+    look a little futher ahead and see if we need to do anything special to the file we're editing: this lets us choose to ban certain sections from being edited easier. 
+
+
+    """
+    abort(500)
---
+++ runserver.py
@@ -0,0 +1,17 @@
+"""
+Alexandria development server
+"""
+
+from os import environ
+from LibServer import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+
+    app.debug = True
+    app.config.from_json("admin.json", silent=True);
+    app.run(HOST, PORT)
