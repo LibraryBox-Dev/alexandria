beb75d9281126f0da370fc4ab14b6cef6fc0867f
Morgan Gangwere <morgan.gangwere@gmail.com>
2017-06-16 18:03:49 -0700

    Cleanup from blueprints

--- runserver.py
+++ runserver.py
@@ -13,14 +13,22 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
 
+    print("LibServer lives in {0}".format(LibServer.__file__))
+
     parser = ArgumentParser("libserv",description="The Alexandria library service",add_help=True)
     parser.add_argument("-localconfig",metavar="file",required=True, type=str,help="Local (user) configuration")
     parser.add_argument("-baseconfig",metavar="file",required=True,  type=str,help="System (default) configuration")
     parser.add_argument("-debug",action='store_true',help="run the server in debug mode")
     parser.add_argument("-host",type=str,default='localhost',help="Bind to this specific host")
     parser.add_argument("-port",type=int,default=5555)
+    parser.add_argument("-no-admin",action='store_true',help="Disable administration panel")
+    
     parser.allow_abbrev=False
     args = parser.parse_args()
+
+
+    app.config["product"] = "PriateBox"
+    app.config["version"] = "3.0-NG"
 
     HOST = environ.get('SERVER_HOST', args.host)
     try:
