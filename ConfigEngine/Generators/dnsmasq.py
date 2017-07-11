25ee4987649a03fffb83d64a00bfb36892f0a6fd
Morgan Gangwere <morgan.gangwere@gmail.com>
2017-07-11 16:41:21 -0700

    Move DNSMasq and Hostapd into services just for simplicity.
    This means we can now simply walk the train for each configured service, no special
    options needed.

--- ConfigEngine/Generators/dnsmasq.py
+++ ConfigEngine/Generators/dnsmasq.py
@@ -2,24 +2,24 @@ from ConfigEngine import toBool,writeLines
 from ConfigEngine.Generators import engine
 
 @engine.assertConfig("general",["hostname"])
-@engine.assertConfig("network", [ 
-    "dnsmasq_enable",
-    "dnsmasq_interface",
-    "dnsmasq_start_address",
-    "dnsmasq_end_address",
-    "dnsmasq_subnet_mask",
-    "dnsmasq_lease_time"
+@engine.assertConfig("service.dnsmasq", [ 
+    "enable",
+    "interface",
+    "start_address",
+    "end_address",
+    "subnet_mask",
+    "lease_time"
     ])
 @engine.generator("DNSMasq DHCP and DNS server")
 def dnsmasq(buffer):
 
-    network = engine.getSection("network")
-    if not toBool(network["dnsmasq_enable"]):
+    dnsmasq_opts = engine.getSection("service.dnsmasq")
+    if not toBool(dnsmasq_opts["enable"]):
         return
 
     writeLines(buffer,[
-        "interface={0}".format(network["dnsmasq_interface"]),
-        "dhcp-range={dnsmasq_start_address},{dnsmasq_end_address},{dnsmasq_subnet_mask},{dnsmasq_lease_time}".format_map(network),
+        "interface={0}".format(dnsmasq_opts["interface"]),
+        "dhcp-range={start_address},{end_address},{subnet_mask},{lease_time}".format_map(dnsmasq_opts),
         "local=/{0}/".format(engine.getOption("general","tld")),
         "dhcp-option=3"
         ])
@@ -29,12 +29,12 @@ def dnsmasq(buffer):
 
     # Check that the section for the interface exists
 
-    if not engine.hasSection("interface."+network["dnsmasq_interface"]):
+    if not engine.hasSection("interface."+dnsmasq_opts["interface"]):
         raise ValueError("Interface for dnsmasq is not defined.")
-    if not engine.hasOption("interface."+network["dnsmasq_interface"], "dhcp") or engine.getOption("interface."+network["dnsmasq_interface"],"dhcp",toBool):
+    if not engine.hasOption("interface."+dnsmasq_opts["interface"], "dhcp") or engine.getOption("interface."+dnsmasq_opts["interface"],"dhcp",toBool):
         raise ValueError("Interface for dnsmasq does not define DHCP option or is configured for dhcp")
     # we want the IP address of the interface.
-    ipAddr = engine.getOption("interface."+network["dnsmasq_interface"], "ip")
+    ipAddr = engine.getOption("interface."+dnsmasq_opts["interface"], "ip")
     writeLines(buffer, ["address=/{0}/{1}".format(
         engine.getOption("general","hostname")+"."+engine.getOption("general","tld"),
         ipAddr
