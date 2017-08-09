8e3e3f4d6b7b552d9013f3042384065398c89f80
Morgan Gangwere <morgan.gangwere@gmail.com>
2017-08-09 10:06:17 -0700

    pep8: runserver

--- bin/runserver.py
+++ bin/runserver.py
@@ -7,7 +7,8 @@ Alexandria development server
 from os import environ
 import LibServer
 from LibServer import app
-import random, string
+import random
+import string
 from LibServer.admin import admin
 from LibServer.browser import browser
 from pidfile import PIDFile
@@ -15,32 +16,35 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser("libserv",description="The Alexandria library service",add_help=True)
-    parser.add_argument("-localconfig",metavar="file",required=True, type=str,help="Local (user) configuration")
-    parser.add_argument("-baseconfig",metavar="file",required=True,  type=str,help="System (default) configuration")
-    parser.add_argument("-debug",action='store_true',help="run the server in debug mode")
-    parser.add_argument("-host",type=str,default='localhost',help="Bind to this specific host")
-    parser.add_argument("-port",type=int,default=5555)
-    parser.add_argument("-no-admin",dest="noadmin",action='store_true',help="Disable administration panel")
-    parser.add_argument("-pidfile",help="PIDFile to lock against", default="libsrv.pid")
+    parser = ArgumentParser("libserv",
+                            description="The Alexandria library service",
+                            add_help=True)
+    parser.add_argument("-localconfig", metavar="file", required=True,
+                        type=str, help="Local (user) configuration")
+    parser.add_argument("-baseconfig", metavar="file", required=True,
+                        type=str, help="System (default) configuration")
+    parser.add_argument("-debug", action='store_true',
+                        help="run the server in debug mode")
+    parser.add_argument("-host", type=str, default='localhost',
+                        help="Bind to this specific host")
+    parser.add_argument("-port", type=int, default=5555,
+                        help="Port to bind to (default 5555)")
+    parser.add_argument("-no-admin", dest="noadmin", action='store_true',
+                        help="Disable administration panel")
+    parser.add_argument("-pidfile", help="PIDFile to lock against",
+                        default="libsrv.pid")
 
-    parser.add_argument("-fcgisocket",type=str,default=None,help="FastCGI socket path.")
-
-    parser.allow_abbrev=False
+    parser.allow_abbrev = False
     args = parser.parse_args()
-
 
     app.config["product"] = "PriateBox"
     app.config["version"] = "3.0-NG"
-    
-
 
     HOST = environ.get('SERVER_HOST', args.host)
     try:
         PORT = int(environ.get('SERVER_PORT', args.port))
     except ValueError:
         PORT = args.port
-
 
     # We're going to get our default and overlay configurations.
     # This is being supplied by the command line
@@ -51,20 +55,20 @@ if __name__ == '__main__':
 
     app.config.load(args.baseconfig, args.localconfig)
 
-    app.config["product"] = app.config.get("alexandria","product", "Alexandria")
+    app.config["product"] = app.config.get(
+        "alexandria", "product", "Alexandria")
     app.config["version"] = app.config.get("alexandria", "version", "(HEAD?)")
 
-    app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
-
-
+    app.secret_key = ''.join(
+        random.choice(string.ascii_uppercase + string.digits)
+        for _ in range(32)
+    )
     app.debug = args.debug
 
     app.register_blueprint(browser)
     if(not args.noadmin):
-       app.register_blueprint(admin,url_prefix='/admin')
-
-    
-    if( app.debug):
+        app.register_blueprint(admin, url_prefix='/admin')
+    if(app.debug):
         print("Can't run with PIDfile and debug; ignoring pidfile")
         app.run(HOST, PORT)
     else:
