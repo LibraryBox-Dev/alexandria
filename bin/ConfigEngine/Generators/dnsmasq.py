78f34abe50c6d14fa0a14c96154a148828c6dae3
Morgan Gangwere <morgan.gangwere@gmail.com>
2017-08-22 00:14:34 -0600

    dnsmasq: According to StackOverflow, this should make iOS Devices do what we want????

--- bin/ConfigEngine/Generators/dnsmasq.py
+++ bin/ConfigEngine/Generators/dnsmasq.py
@@ -17,10 +17,21 @@ def dnsmasq(buffer):
     if not toBool(dnsmasq_opts["enabled"]):
         return
 
+
+
+    # If you're wondering why this looks bad, it's because it is.
+    # Apple is terrible and should feel bad.
+    # see VVVVVVVV THIS STACK OVERFLOW ANSWER
+    # https://apple.stackexchange.com/questions/62870/how-do-i-tell-an-ios-device-theres-no-internet-connection-on-the-wifi/62905#62905
+    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    # Apparently, if you set the default route to 0.0.0.0 then iOS gets confused and doesn't freak out????
+    #
+    # dhcp-option=15 sends an empty "domain" entry. This is another attempt to pull the wool over Apple's eyes.
+
     writeLines(buffer,[
         "interface={0}".format(dnsmasq_opts["interface"]),
         "dhcp-range={start_address},{end_address},{subnet_mask},{lease_time}".format_map(dnsmasq_opts),
-        "dhcp-option=3",
+        "dhcp-option=3,0.0.0.0",
         "dhcp-option=15"
         ])
 
